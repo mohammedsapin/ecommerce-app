fa106a60f6fe9d9d5f691b972e19b1e505837976
Mohammed Sapin <sapin.mohammed@gmail.com>
2019-07-05 16:11:32 -0400

    checkout page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import { createStructuredSelector } from 'reselect'
 
 import "./App.css";
 
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Account from "./pages/account/account.component";
+import CheckoutPage from "./pages/checkout/checkout.component.jsx"
+
 import Header from "./components/header/header.component";
+
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+
 import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentuser } from './redux/user/user.selectors'
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -44,6 +50,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path = '/checkout' component={CheckoutPage} />
           <Route
             exact
             path="/signin"
@@ -61,8 +68,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser,
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentuser,
 });
 
 const mapDispatchToProps = dispatch => ({
